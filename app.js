6729aa2b0873fd1cdc256707cb49daa3b7b1759d
Yury Amonsky <yury.amonsky@yandex.ru>
2022-10-14 00:56:55 +0300

    feat: добавить схему, контроллеры, роуты для /users

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const users = require('./routes/users');
+
+const app = express();
 const { PORT = 3000 } = process.env;
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(users);
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`)
-})
+/* eslint no-console: ["error", { allow: ["log"] }] */
+  console.log(`App listening on port ${PORT}`);
+});
---
+++ controllers/users.js
@@ -0,0 +1,23 @@
+/* eslint no-underscore-dangle: ["error", { "allow": ["_id"] }] */
+const User = require('../models/user');
+
+module.exports.getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+module.exports.getUser = (req, res) => {
+  User.findById(req.params._id)
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+module.exports.createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+  /* eslint no-console: ["error", { allow: ["log"] }] */
+  console.log(req.body);
+  User.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
